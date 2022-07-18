18fec71cdab1b9dc493ed94c389db7943e67e729
BesokDev <74706532+BesokDev@users.noreply.github.com>
2022-07-18 16:56:17 +0200

    create AdminController

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+/**
+ * @Route("/admin")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/tableau-de-bord", name="show_dashboard", methods={"GET"})
+     */
+    public function showDashboard(EntityManagerInterface $entityManager): Response
+    {
+        return $this->render("admin/show_dashboard.html.twig");
+    }
+
+    public function createArticle(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    {
+        
+    }
+}
---
+++ src/Form/ArticleFormType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+            ])
+            ->add('subtitle', TextType::class, [
+                'label' => 'Sous-titre'
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu'
+            ])
+            ->add('photo', FileType::class, [
+                'label' => 'Photo',
+                'data_class' => null,
+                'attr' => [
+                    'data-default-file' => $options['photo']
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider',
+                'validate' => false,
+                'attr' => [
+                    'class' => 'd-block mx-auto my-3 col-3 btn btn-success'
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+            'allow_file_upload' => true,
+            'photo' => null
+        ]);
+    }
+}
